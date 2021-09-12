a2cfbdf6ea6e3bb25eddea8029578e78d92754f5
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-09-12 12:32:05 +0300

    Create gunicorn config for django app

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-gunicorn -b :8080 hello:app
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+#sudo /etc/init.d/gunicorn restart
+#gunicorn -b :8080 hello:app
+sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
