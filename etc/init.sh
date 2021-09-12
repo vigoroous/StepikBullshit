5da8bb454654582d3ee6cdb65bf6e66503ec2484
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-09-12 10:45:45 +0300

    WSGI task

--- etc/init.sh
+++
@@ -1,7 +0,0 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-gunicorn -b :8080 hello:app
