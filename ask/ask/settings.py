9f8ab0796d2ddae216c269a5a9df31b653caf81c
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-09-16 00:38:03 +0300

    Still trying to solve MySQL connection on Stepik

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = { 
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.mysql',
 #        'NAME': 'web',
@@ -90,8 +90,15 @@ DATABASES = {
 #        'HOST': 'localhost',
 #        'PORT': '',
 #    }
-#}
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stepic_web',
+        'USER': 'box',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- initdb.sh
+++ initdb.sh
@@ -1,12 +1,16 @@
 sudo /etc/init.d/mysql start
 
-mysql -uroot -e "create database web;"
+# mysql -uroot -e "create database web;"
 
-mysql -uroot -e "create user 'box'@'localhost' identified by '1234';"
+# mysql -uroot -e "create user 'box'@'localhost' identified by '1234';"
 
-mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+# mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 
-cd ~/web/ask
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+cd /home/box/web/ask
 
 python3 manage.py makemigrations qa
 
