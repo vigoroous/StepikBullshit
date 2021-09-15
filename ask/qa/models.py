a47274a63bc286979ff0a78ebd3a5e7cd0b2260c
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-09-16 00:24:57 +0300

    Patch Question model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, default= 'x')
+    author = models.ForeignKey(User, default= '1', on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
 class Answer(models.Model):
