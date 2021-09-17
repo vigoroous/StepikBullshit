288db0c065d358aa4bbfc2e10cb7f642d80b41aa
Bogdan Martyshevskiy <vigoroous@gmail.com>
2021-09-17 20:48:04 +0300

    Cleanup project

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
+sudo /etc/init.d/mysql start
+
+# mysql -uroot -e "create database web;"
+# mysql -uroot -e "create user 'box'@'localhost' identified by '1234';"
+# mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+cd /home/box/web/ask
+python3 manage.py makemigrations qa
+python3 manage.py migrate
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
@@ -7,5 +20,6 @@ sudo /etc/init.d/nginx restart
 #gunicorn -b :8080 hello:app
 #sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
 #sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
-cd ~/web/ask
+
+cd /home/box/web/ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi:application
